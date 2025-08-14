60a974641034d916e0d52fa45bd64b38235967ba
Daredevil-sys <qq4507141@gmail.com>
2025-08-13 21:16:24 -0600

    App5Fabricio

---
+++ EmpresaImportadora/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpresaImportadora
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EmpresaImportadora/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace EmpresaImportadora
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularDolares(object sender, EventArgs e)
+        {
+            if (double.TryParse(pesosEntry.Text, out double pesos) &&
+                double.TryParse(tipoCambioEntry.Text, out double tipoCambio) &&
+                tipoCambio > 0)
+            {
+                double dolares = pesos / tipoCambio;
+                resultadoLabel.Text = $"Con ${pesos:F2} MXN puedes adquirir ${dolares:F2} USD";
+            }
+            else
+            {
+                resultadoLabel.Text = "Por favor, ingrese valores válidos.";
+            }
+        }
+
+    }
+}
