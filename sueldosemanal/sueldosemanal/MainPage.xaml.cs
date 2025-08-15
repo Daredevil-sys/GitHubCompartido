90286e71b4c7526a9916626dfe0a75cd2e21a515
CarmenBarton <carmenbarton11@gmail.com>
2025-08-14 20:52:51 -0600

    CarmenApp4

---
+++ sueldosemanal/sueldosemanal/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace sueldosemanal
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalcularClicked(object sender, EventArgs e)
+        {
+            // Validar entrada
+            if (double.TryParse(txtHoras.Text, out double horas) &&
+                double.TryParse(txtPagoHora.Text, out double pagoHora))
+            {
+                // Calcular sueldo semanal
+                double sueldo = horas * pagoHora;
+                lblResultado.Text = $"Sueldo semanal: ${sueldo:F2}";
+            }
+            else
+            {
+                lblResultado.Text = "⚠️ Ingresa valores válidos.";
+            }
+        }
+    }
+}
