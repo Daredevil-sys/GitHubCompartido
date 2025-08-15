ff06078e7733f06bd8f0bd2bab4a313ecba4b21f
Daredevil-sys <qq4507141@gmail.com>
2025-08-14 20:13:54 -0600

    FabricioApp6

---
+++ Practica 10/LangostaAhumada/LangostaAhumada/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace LangostaAhumada
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularPresupuesto(object sender, EventArgs e)
+        {
+            if (int.TryParse(personasEntry.Text, out int cantidadPersonas) && cantidadPersonas > 0)
+            {
+                double costoPorPersona;
+
+                if (cantidadPersonas <= 200)
+                    costoPorPersona = 95;
+                else if (cantidadPersonas <= 300)
+                    costoPorPersona = 85;
+                else
+                    costoPorPersona = 75;
+
+                double total = cantidadPersonas * costoPorPersona;
+
+                resultadoLabel.Text = $"Costo por persona: ${costoPorPersona:F2}\n" +
+                                      $"Presupuesto total: ${total:F2}";
+            }
+            else
+            {
+                resultadoLabel.Text = "Por favor, ingrese un número válido de personas.";
+            }
+        }
+
+
+    }
+}
